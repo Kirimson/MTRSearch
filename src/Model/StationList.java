b4c981a9cf636a61cedc8cc22becc8d9771a33a1
Kirimson <kierangates@outlook.com>
2017-12-14 19:01:33 +0000

    Comment BFS and remove redudundant methods

--- src/Model/StationList.java
+++ src/Model/StationList.java
@@ -18,67 +18,75 @@ public class StationList {
 	}
 	
 	/**
-	 * This method add {@link Station} to the stationList hashmap
-	 * @param list is the stations in the hashmap
+	 * This method add {@link Station} to the stationList HashMap
+	 * @param list is the stations in the HashMap
 	 */
 	public void addStations(HashMap<String, Station> list) {
 		stationList = list;
 	}
 	
 	/**
-	 * This mehod retrieves the {@link Station} name searched
+	 * Retrieves the {@link Station} specified by the name parameter
 	 * @param name of station
-	 * @return station name in lowercase
+	 * @return {@link Station} object specified
 	 */
 	public Station getStation(String name) {
 		return stationList.get(name.toLowerCase());
 	}
 	
 	/**
-	 * This method retrieves the values in the code
-	 * @return the values from the stationList array
-	 */
-	public Collection<Station> getValues(){
-		return stationList.values();
-	}
-	
-	/**
-	 * This method reads out the name of {@link Station} objects in the object 
+	 * Finds the {@link Station} specified as goal from the {@link Station} specified as start.
+	 * Uses Breadth First Search to find the goal station
 	 * @param start is the station the path is starting from
 	 * @param goal is the end of the path
-	 * @return path
+	 * @return HashMap containing a station, and the station the algorithm used last to get to this station
 	 */
 	public HashMap<Station, ArrayList<Station>> findPath(Station start, Station goal){
 		
+		//create open and closed sets for search data 
 		LinkedList<Station> openSet = new LinkedList<Station>();
 		HashSet<Station> closedSet = new HashSet<Station>();
 		HashMap<Station, ArrayList<Station>> path = new HashMap<Station, ArrayList<Station>>();
 		
+		//add the starting station to the open set (to search)
 		openSet.add(start);
 		
+		//while there are still stations to be searched
 		while(!openSet.isEmpty()) {
+			
+			//pop the current station from the open set
 			Station current = openSet.pop();
 			
+			//for every station that is linked to the current station
 			for(Station linkedStation : current.getLinkedStations()) {
+				
+				//if this station has already been fully searched, don't search it
 				if(closedSet.contains(linkedStation))
 					continue;
 				
+				//if this station hasn't been looked at before, set up the ArrayList value for this station's parents
 				if(path.get(linkedStation) == null)
 					path.put(linkedStation, new ArrayList<Station>());
-					
+				
+				//if the linked station is the goal
 				if(linkedStation == goal) {
+					//add the current station (the parent) to the ArrayList
 					path.get(linkedStation).add(current);
-					openSet.removeAll(openSet);
 					return path;
 				}
 				
+				//if the open set does not contain this station already
 				if(openSet.contains(linkedStation) == false) {
+					//add the current station (the parent) to the ArrayList
 					path.get(linkedStation).add(current);
+					
+					//add this station to the open set, to be searched later
 					openSet.add(linkedStation);
 				}
 				
 			}
 			
+			//add the current station to the closed set after it has been fully searched
 			closedSet.add(current);
 			
 		}
